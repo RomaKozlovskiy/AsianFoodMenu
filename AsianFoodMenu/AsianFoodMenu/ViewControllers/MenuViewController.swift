18405f0e94975455e748e46dd1201f5a87dca4a0
RomanKozlovskiy <roma.kozlovskiy.96@mail.ru>
2023-09-21 14:55:00 +0300

    initial project settings

---
+++ AsianFoodMenu/AsianFoodMenu/ViewControllers/MenuViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AsianFoodMenu
+//
+//  Created by Роман Козловский on 21.09.2023.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
